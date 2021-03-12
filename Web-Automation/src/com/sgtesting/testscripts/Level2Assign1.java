152f895dd0a58fe1027e2b011364dc6a79e786f4
NATHILA99 <nathilaanjumg1999@gmail.com>
2021-03-12 15:37:00 +0530

    AutomationDraft Mar12-1

---
+++ Web-Automation/src/com/sgtesting/testscripts/Level2Assign1.java
@@ -0,0 +1,158 @@
+package com.sgtesting.testscripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Level2Assign1 {
+
+	public static WebDriver oBrowser=null;
+	public static void main(String[] args) {
+		launchBrowser();
+		navigate();
+		login();
+		minimizeFlyOutWindow();
+		createUser();
+		logout();
+		login1();
+		createUser2();
+		modifyPassword();
+		logout1();
+
+	}
+	
+	public static void launchBrowser()
+	{
+		try	
+		{
+			String path=System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver",path+"\\Library\\driver\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void navigate()
+	{
+		try
+		{
+			oBrowser.get("http://localhost:82/login.do");
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	static void login()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys("admin");
+			oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+			oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+			Thread.sleep(5000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	static void minimizeFlyOutWindow()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	static void createUser()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='topnav']/tbody/tr[1]/td[5]/a/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id='createUserDiv']/div/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.name("firstName")).sendKeys("name1");
+			oBrowser.findElement(By.name("lastName")).sendKeys("user");
+			oBrowser.findElement(By.name("email")).sendKeys("user1@gmail.com");
+			oBrowser.findElement(By.name("username")).sendKeys("User1");
+			oBrowser.findElement(By.name("password")).sendKeys("Sample1");
+			oBrowser.findElement(By.name("passwordCopy")).sendKeys("Sample1");
+			oBrowser.findElement(By.xpath("//*[@id='userDataLightBox_commitBtn']/div/span")).click();
+			Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+	static void logout()
+	{
+		try
+		{
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	static void login1()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys("User1");
+			oBrowser.findElement(By.name("pwd")).sendKeys("Sample1");
+			oBrowser.findElement(By.xpath("//*[@id=\'loginButton\']/div")).click();
+			Thread.sleep(5000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	static void createUser2()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='topnav']/tbody/tr[1]/td[5]/a/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id='createUserDiv']/div/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.name("firstName")).sendKeys("name2");
+			oBrowser.findElement(By.name("lastName")).sendKeys("user");
+			oBrowser.findElement(By.name("email")).sendKeys("user1@gmail.com");
+			oBrowser.findElement(By.name("username")).sendKeys("User2");
+			oBrowser.findElement(By.name("password")).sendKeys("Sample2");
+			oBrowser.findElement(By.name("passwordCopy")).sendKeys("Sample2");
+			oBrowser.findElement(By.xpath("//*[@id='userDataLightBox_commitBtn']/div/span")).click();
+			Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+	static void modifyPassword()
+	{
+			oBrowser.findElement(By.xpath("//*[@id='topnav']/tbody/tr[1]/td[5]/a/div[2]")).click();
+			oBrowser.findElement(By.name("password")).sendKeys("gubbi");
+			oBrowser.findElement(By.name("passwordCopy")).sendKeys("gubbi");
+			oBrowser.findElement(By.xpath("//*[@id=\'userDataLightBox_commitBtn\']/div/span")).click();
+			
+	}
+	static void logout1()
+	{
+		try
+		{
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
